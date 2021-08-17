37acb7f1cb035841083873dd8fe54be2893aff7e
Vicky BRS <viki2711@gmail.com>
2021-08-17 11:46:27 -0400

    Django set up

--- djproject/djproject/settings.py
+++ djproject/djproject/settings.py
@@ -68,8 +68,11 @@ WSGI_APPLICATION = 'djproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbrealtor',
+        'USER': 'postgres',
+        'PASSWORD': 'vikidb2711',
+        'HOST': 'localhost'
     }
 }
 
@@ -120,3 +123,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
