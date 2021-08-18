20950c6e3da94b84fdb41c0b28ce46dfddbe7bde
Vicky BRS <viki2711@gmail.com>
2021-08-18 16:37:13 -0400

    Add search page

--- djproject/listings/views.py
+++ djproject/listings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import Paginator
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 
 def index(request):
@@ -18,8 +19,21 @@ def index(request):
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    context = {
+        'state_choices': state_choices,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices
+    }
+
+    return render(request, 'listings/search.html', context)
