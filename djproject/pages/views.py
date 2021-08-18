3813b86090a6f2afe4e68b30bf9f8023e6625468
Vicky BRS <viki2711@gmail.com>
2021-08-18 14:52:09 -0400

    Add dynamic about page

--- djproject/pages/views.py
+++ djproject/pages/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from listings.models import Listing
+from realtors.models import Realtor
 
 
 def index(request):
+    # Get all listings
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
     context = {
@@ -12,4 +14,14 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    # Get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+    return render(request, 'pages/about.html', context)
